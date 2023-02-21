2373709ec98ce3be4c643584b3c2ae0e4a1723e1
MohamedRElDeeb <moh.rafik.eldeeb@gmail.com>
2023-02-21 15:15:35 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,8 @@
  * is written by mbah
  * Return: 0
  */
-
 int main(void)
- {
+{
 	 _putchar('_');
 	 _putchar('p');
 	 _putchar('u');
@@ -17,4 +16,4 @@ int main(void)
 	 _putchar('r');
 	 _putchar('\n');
 	 return (0);
- }
+}
