ac68344e73fba7d058b52e771bdaa3a2892e1648
MohamedRElDeeb <moh.rafik.eldeeb@gmail.com>
2023-03-12 14:30:15 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Etry point
+ * Print single digits of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
